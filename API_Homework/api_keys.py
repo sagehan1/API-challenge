ab1696468c17d20995c8aa40b528893fe540793d
Sage Hanson <sage.hanson1@outlook.com>
2025-02-26 21:31:44 -0600

    Initial Commit

---
+++ API_Homework/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "741bce72986fc1632b74564af6db40ba"
+
+# Geoapify API Key
+geoapify_key = "38b36aab9e5a44b7b3ccbe3ef5a2cd04"
---
+++ API_Homework/code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "741bce72986fc1632b74564af6db40ba"
+
+# Geoapify API Key
+geoapify_key = "4d581d69ce054afaaa40acd1d1c5e0d9"
